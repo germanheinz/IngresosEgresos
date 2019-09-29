dc4984213a8e5e6ce7ca9e227d409cacbe4d9832
germanheinz <heinz.german@gmail.com>
2019-09-29 22:11:34 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDo0wfys8xRYsv_8BaQ4c1EKouzMvPr5tM',
+    authDomain: 'ingreso-egreso-app-7c3a2.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-7c3a2.firebaseio.com',
+    projectId: 'ingreso-egreso-app-7c3a2',
+    storageBucket: 'ingreso-egreso-app-7c3a2.appspot.com',
+    messagingSenderId: '357494733865',
+    appId: '1:357494733865:web:f44f650026a8ade6'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDo0wfys8xRYsv_8BaQ4c1EKouzMvPr5tM',
+    authDomain: 'ingreso-egreso-app-7c3a2.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-7c3a2.firebaseio.com',
+    projectId: 'ingreso-egreso-app-7c3a2',
+    storageBucket: 'ingreso-egreso-app-7c3a2.appspot.com',
+    messagingSenderId: '357494733865',
+    appId: '1:357494733865:web:f44f650026a8ade6'
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
